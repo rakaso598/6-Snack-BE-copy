a8af2f2a19f2ff63e19a494361ce2303d0963fa2
rakaso598 <nexonaddress98@gmail.com>
2025-07-09 17:39:43 +0900

    refactor: inviteRouter, userRouter 임포트경로 추가

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -4,15 +4,10 @@ import cartRouter from "./cart.route";
 import orderRequestRouter from "./orderRequest.route";
 import adminRouter from "./admin.route";
 import superAdminRouter from "./superAdmin.route";
-<<<<<<< HEAD
-import cartRouter from "./cart.route";
-import authRouter from "./auth.route";
-import inviteRouter from "./invite.route";
-import userRouter from "./user.route";
-=======
 import productRouter from './product.route'
 import myRouter from './my.route';
->>>>>>> 912d7162c0fed2cfa798108dd27170693f91e7f8
+import inviteRouter from "./invite.route";
+import userRouter from "./user.route";
 
 const indexRouter = Router();
 
