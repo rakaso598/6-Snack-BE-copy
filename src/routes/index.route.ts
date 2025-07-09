09a41340f0f5813243117cecadc7dc9827a995ed
Jam1eL1 <23jisu@gmail.com>
2025-07-09 23:23:37 +0900

    chore: index.route 임시주석처리

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -4,22 +4,21 @@ import cartRouter from "./cart.route";
 import orderRequestRouter from "./orderRequest.route";
 import adminRouter from "./admin.route";
 import superAdminRouter from "./superAdmin.route";
-import productRouter from './product.route'
-import myRouter from './my.route';
+import productRouter from "./product.route";
+import myRouter from "./my.route";
 import inviteRouter from "./invite.route";
 import userRouter from "./user.route";
 
 const indexRouter = Router();
 
 indexRouter.use("/super-admin", superAdminRouter);
-indexRouter.use("/admin", adminRouter);
-indexRouter.use("/products", productRouter);
+// indexRouter.use("/admin", adminRouter);
+// indexRouter.use("/products", productRouter);
 indexRouter.use("/cart", cartRouter);
 // indexRouter.use("/orders", orderRouter);
-indexRouter.use("/my", myRouter);
+// indexRouter.use("/my", myRouter);
 indexRouter.use("/auth", authRouter);
-indexRouter.use("/invite", inviteRouter);
-indexRouter.use("/users", userRouter);
-
+// indexRouter.use("/invite", inviteRouter);
+// indexRouter.use("/users", userRouter);
 
 export default indexRouter;
