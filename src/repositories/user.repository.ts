a17585806efc2238525354ad26b52530df49d9e9
Jam1eL1 <23jisu@gmail.com>
2025-07-09 18:34:41 +0900

    feat: 유저 탈퇴 기능 서비스, 레포지토리 레이어 작성

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,1 +1,28 @@
-import prisma from '../config/prisma';
+import { Prisma } from "@prisma/client";
+import prisma from "../config/prisma";
+
+const findActiveUserById = async (id: string) => {
+  return await prisma.user.findUnique({
+    where: { id, deletedAt: null },
+    // 비밀번호, refreshToken 정보 제외
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      role: true,
+      companyId: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+// 유저 삭제 (soft delete)
+const deleteUser = async (id: string ) => {
+  return await prisma.user.update({
+    where: { id },
+    data: { deletedAt: new Date() },
+  });
+};
+
+export default { findActiveUserById, deleteUser };
---
+++ src/services/user.service.ts
@@ -0,0 +1,21 @@
+import userRepository from "../repositories/user.repository";
+import { BadRequestError, NotFoundError } from "../types/error";
+
+const deleteUser = async (userId: string, currentUser: any) => {
+  // 삭제 하려는 유저 조회
+  const userToDelete = await userRepository.findActiveUserById(userId);
+  if (!userToDelete) {
+    throw new NotFoundError("유저가 존재하지 않습니다");
+  }
+
+  // 최고 관리자가 자기 자신을 삭제할수 없음
+  if (userToDelete.id === currentUser.id) {
+    throw new BadRequestError("최고 관리자는 자기 자신을 삭제할 수 없습니다.");
+  }
+
+  // 단순히 User만 soft delete
+  // 코드잇에 따르면 유저가 삭제되어도 삭제 이전에 남겨둔 구매 요청, 등록한 상품 등은 다 남아있어야함.
+  return await userRepository.deleteUser(userToDelete.id);
+};
+
+export default { deleteUser };
