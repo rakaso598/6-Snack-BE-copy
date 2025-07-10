72ef027193f9071b4cacae597c28054560103fa1
Jam1eL1 <23jisu@gmail.com>
2025-07-10 11:07:00 +0900

    feat: 유저 권한 변경 엣지케이스 테스트 완료 & 서비스에 로직추가

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -27,9 +27,13 @@ const updateRole = async (userId: string, role: UserRole, currentUser: TCurrentU
   if (!userToUpdateRole) {
     throw new NotFoundError("유저가 존재하지 않습니다");
   }
+  if (role !== "ADMIN" && role !== "USER") {
+    throw new BadRequestError("잘못된 Role 값입니다.");
+  }
   if (userToUpdateRole.id === currentUser.id) {
     throw new BadRequestError("최고 관리자는 자기 자신의 권한을 변경할 수 없습니다.");
   }
+
   return await userRepository.updateUserRole(userId, role);
 };
 
