82a2a241514e50fdb233b1ac4eacd87ea45e20de
rakaso598 <112613372+rakaso598@users.noreply.github.com>
2025-07-11 23:58:00 +0900

    feat: 인증요청을 위한 타입 정의 및 사용

--- src/middlewares/jwtAuth.middleware.ts
+++ src/middlewares/jwtAuth.middleware.ts
@@ -3,8 +3,20 @@ import jwt from "jsonwebtoken";
 import { Role } from "@prisma/client";
 import { AuthenticationError, NotFoundError, ServerError } from "../types/error";
 import prisma from "../config/prisma";
+import { Prisma } from "@prisma/client";
+import { TAuthenticatedRequest } from "../types/AuthenticatedRequest.type";
 
 const JWT_SECRET: string = process.env.JWT_SECRET ?? "your_very_strong_jwt_secret_key_please_change_this_in_production";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: Prisma.UserGetPayload<{
+        include: { company: true };
+      }>;
+    }
+  }
+}
 
 /**
  * 토큰 인증 미들웨어
