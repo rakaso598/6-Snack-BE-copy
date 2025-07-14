32180a0facfaa3cc2db98cce5be61a41ff094609
Jam1eL1 <23jisu@gmail.com>
2025-07-14 16:05:26 +0900

    chore: ordered-item 목데이터 업데이트 (productId 필드 추가로 인한)

--- prisma/mocks/ordered-item.mock.ts
+++ prisma/mocks/ordered-item.mock.ts
@@ -1,12 +1,12 @@
 export const orderedItemMockData = [
-  // orderId: 1, user-1
-  { id: 1, orderId: 1, receiptId: 1 },
-  { id: 2, orderId: 1, receiptId: 2 },
+  // orderId: 1, user-1 (오리지널 포카칩 2개 + 코카콜라 1개)
+  { id: 1, orderId: 1, receiptId: 1, productId: 1 }, // 오리지널 포카칩
+  { id: 2, orderId: 1, receiptId: 2, productId: 6 }, // 코카콜라 500ml
 
-  // orderId: 2, user-2
-  { id: 3, orderId: 2, receiptId: 3 },
+  // orderId: 2, user-2 (킷캣 미니 3개)
+  { id: 3, orderId: 2, receiptId: 3, productId: 3 }, // 킷캣 미니
 
-  // orderId: 3, user-3
-  { id: 4, orderId: 3, receiptId: 4 },
-  { id: 5, orderId: 3, receiptId: 5 },
+  // orderId: 3, user-3 (신라면 컵 1개 + 아몬드 믹스 1개)
+  { id: 4, orderId: 3, receiptId: 4, productId: 9 }, // 신라면 컵
+  { id: 5, orderId: 3, receiptId: 5, productId: 4 }, // 아몬드 믹스
 ];
