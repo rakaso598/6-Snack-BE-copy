d7687e072c3cb966b9babd4271a62a43cadc1f1b
Jam1eL1 <23jisu@gmail.com>
2025-07-24 21:59:52 +0900

    fix: 오타수정, nextCursor은 숫자로 response에 넣도록 수정

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -82,7 +82,6 @@ const getProducts: RequestHandler<{}, {}, {}, TGetProductsQueryDto> = async (req
     const nextCursor = items.length > 0 ? items[items.length - 1].id : null;
 
     res.json({ items, nextCursor: nextCursor ? nextCursor.toString() : null });
-    nextCursor;
   } catch (error) {
     next(error instanceof Error ? error : new ServerError("예기치 못한 에러", error));
   }
