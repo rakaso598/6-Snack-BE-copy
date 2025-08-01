6ba4a45a8bd5baa62abec0d823d06bb5ab146d81
Wooju <dnwn810@naver.com>
2025-08-02 00:01:13 +0900

    fix: 쓸모 없을 것 같은 주석 제거

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -10,6 +10,6 @@ router.post("/", authenticateToken, upload.single("image"), productController.cr
 router.get("/:id", authenticateToken, productController.getProductDetail);
 router.patch("/:id", authenticateToken, productController.updateProduct);
 router.delete("/:id", authenticateToken, productController.deleteProduct);
-router.get("/", authenticateToken, productController.getProducts); // 인증 필수로 변경
+router.get("/", authenticateToken, productController.getProducts);
 
 export default router;
