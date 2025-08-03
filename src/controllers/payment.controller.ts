3fc3cb3633f5ea9ee4c38a08c455c7cf00969a86
Decal <vumublne@naver.com>
2025-08-04 06:26:59 +0900

    refactor: 주석 수정

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -10,16 +10,15 @@ type TConfirmPaymentBodyDto = {
 };
 
 const confirmPayment: RequestHandler<{}, {}, TConfirmPaymentBodyDto> = async (req, res, next) => {
-  // 클라이언트에서 받은 JSON 요청 바디입니다.
   const { paymentKey, orderId, amount } = req.body;
 
-  // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
-  // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
+  // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용 X
+  // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가
   const widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
   const encryptedSecretKey = "Basic " + Buffer.from(widgetSecretKey + ":").toString("base64");
 
   try {
-    // 결제를 승인하면 결제수단에서 금액이 차감돼요.
+    // 결제 승인 시 결제수단에서 금액 차감
     const response = await axios.post(
       "https://api.tosspayments.com/v1/payments/confirm",
       { orderId, amount, paymentKey },
@@ -40,11 +39,9 @@ const confirmPayment: RequestHandler<{}, {}, TConfirmPaymentBodyDto> = async (re
       );
     });
 
-    // 결제 성공 비즈니스 로직을 구현하세요.
     res.status(response.status).json(response.data);
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
-      // 결제 실패 비즈니스 로직을 구현하세요.
       res.status(error.response.status).json(error.response.data);
     } else {
       res.status(500).json({ message: "Internal Server Error" });
