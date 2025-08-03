1b238226ee70947fcf4ad1a1ed3bc89e8305e75e
Decal <vumublne@naver.com>
2025-08-04 06:05:39 +0900

    chore: 스키마 수정(Payment 테이블 추가, orderId uuid로 변경)

---
+++ prisma/migrations/20250803150425_add_payment_table_and_change_order_id_type_uuid/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - The primary key for the `Order` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Receipt" DROP CONSTRAINT "Receipt_orderId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" DROP CONSTRAINT "Order_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Order_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Order_id_seq";
+
+-- AlterTable
+ALTER TABLE "Receipt" ALTER COLUMN "orderId" SET DATA TYPE TEXT;
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "paymentKey" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Receipt" ADD CONSTRAINT "Receipt_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
