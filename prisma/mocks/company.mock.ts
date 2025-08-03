54faf7db18d60a37ed5ef30a30907409f99ab729
Jam1eL1 <23jisu@gmail.com>
2025-08-03 20:21:12 +0900

    refactor: 목데이터 스키마에 맞게 변경

--- prisma/mocks/company.mock.ts
+++ prisma/mocks/company.mock.ts
@@ -1,12 +1,12 @@
 export const companyMockData = [
   {
-    name: "코드잇",
+    name: "토쓰(주)",
     bizNumber: "123-45-67890",
     createdAt: new Date("2024-01-01"),
     updatedAt: new Date("2024-01-01"),
   },
   {
-    name: "스낵코퍼레이션",
+    name: "쿠빵(주)",
     bizNumber: "987-65-43210",
     createdAt: new Date("2024-01-01"),
     updatedAt: new Date("2024-01-01"),
