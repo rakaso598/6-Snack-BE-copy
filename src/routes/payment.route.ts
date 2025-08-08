06078ac4f815fd8e2ad3126ea342fc2deaea05b2
Decal <vumublne@naver.com>
2025-08-08 10:44:46 +0900

    refactor: 테스트 코드 삭제

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 import authenticateToken from "../middlewares/jwtAuth.middleware";
 import paymentController from "../controllers/payment.controller";
 import authorizeRoles from "../middlewares/authorizeRoles.middleware";
-import orderRepository from "../repositories/order.repository";
-import cartRepository from "../repositories/cart.repository";
 
 const paymentRouter = Router();
 
@@ -13,16 +11,5 @@ paymentRouter.post(
   authorizeRoles("ADMIN", "SUPER_ADMIN"),
   paymentController.confirmPayment,
 );
-
-paymentRouter.get("/test", async (req, res, next) => {
-  const userId = "user-1";
-  const { orderId } = req.body;
-  // const order = await orderRepository.getOrderWithCartItemIdsById(orderId);
-
-  // const cartItem = await cartRepository.revertCartItem(userId, productIds);
-  const d = await orderRepository.deleteReceiptAndOrder(orderId);
-
-  res.status(204).json(d);
-});
 
 export default paymentRouter;
