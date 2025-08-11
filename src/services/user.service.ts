05bf125d79f478ad8ac359a809f3706be495ced2
Jam1eL1 <23jisu@gmail.com>
2025-08-11 10:26:49 +0900

    refactor: /me 엔드포인트 응답 변경, 필요하지 않는 함수 제거

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -157,13 +157,11 @@ const getMe = async (userId: string) => {
   if (!user) {
     throw new NotFoundError("사용자 정보를 찾을 수 없습니다. 다시 로그인해 주세요.");
   }
-  const cartItemCount = await userRepository.getCartItemCountByUserId(userId);
   return {
     id: user.id,
     email: user.email,
     name: user.name,
     role: user.role,
-    cartItemCount,
     company: {
       id: user.company.id,
       name: user.company.name,
