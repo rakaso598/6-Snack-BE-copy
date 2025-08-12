78fb3d6e8315167476517dba3ec02b9ab6636103
rakaso598 <nexonaddress98@gmail.com>
2025-08-12 15:21:48 +0900

    feat: my API 라우트에 Redis 캐시미들웨어 적용

--- src/routes/my.route.ts
+++ src/routes/my.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import productController from "../controllers/product.controller";
 import authenticateToken from "../middlewares/jwtAuth.middleware";
-
+import { cacheMiddleware } from '../middlewares/cacheMiddleware';
 
 const router = Router();
-router.get("/products", authenticateToken, productController.getMyProducts);
+router.get("/products", cacheMiddleware(), authenticateToken, productController.getMyProducts);
 
 export default router;
