85a2bc23c040b23f3781294bcff52e52abb3f960
Wooju <dnwn810@naver.com>
2025-08-12 15:25:31 +0900

    refactor:오더랑 페이먼츠 라우트 미들웨어 추가

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authenticateToken from "../middlewares/jwtAuth.middleware";
 import paymentController from "../controllers/payment.controller";
 import authorizeRoles from "../middlewares/authorizeRoles.middleware";
+import { invalidateCache } from "../middlewares/cacheMiddleware";
 
 const paymentRouter = Router();
 
 paymentRouter.post(
   "/confirm",
+  invalidateCache(),
   authenticateToken,
   authorizeRoles("ADMIN", "SUPER_ADMIN"),
   paymentController.confirmPayment,
@@ -14,6 +16,7 @@ paymentRouter.post(
 
 paymentRouter.delete(
   "/cancel",
+  invalidateCache(),
   authenticateToken,
   authorizeRoles("ADMIN", "SUPER_ADMIN"),
   paymentController.cancelPayment,
