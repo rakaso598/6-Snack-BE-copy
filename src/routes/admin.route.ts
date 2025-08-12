1e5df90a573d17431d7bb56df08940144804f231
Decal <vumublne@naver.com>
2025-08-12 15:06:52 +0900

    feat: Redis 적용

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -4,24 +4,28 @@ import authenticateToken from "../middlewares/jwtAuth.middleware";
 import authorizeRoles from "../middlewares/authorizeRoles.middleware";
 import productController from "../controllers/product.controller";
 import budgetController from "../controllers/budget.controller";
+import { cacheMiddleware, invalidateCache } from "../middlewares/cacheMiddleware";
 
 const adminRouter = Router();
 
 adminRouter.use("/orders", adminOrderRouter);
 adminRouter.get(
   "/:companyId/budgets",
+  cacheMiddleware(),
   authenticateToken,
   authorizeRoles("ADMIN", "SUPER_ADMIN"),
   budgetController.getMonthlyBudget,
 );
 adminRouter.delete(
   "/products/:id",
+  invalidateCache(),
   authenticateToken,
   authorizeRoles("ADMIN", "SUPER_ADMIN"),
   productController.forceDeleteProduct,
 );
 adminRouter.patch(
   "/products/:id",
+  invalidateCache(),
   authenticateToken,
   authorizeRoles("ADMIN", "SUPER_ADMIN"),
   productController.forceUpdateProduct,
