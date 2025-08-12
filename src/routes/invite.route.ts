31a789623c92827e0675f934a367e5ea40074264
rakaso598 <nexonaddress98@gmail.com>
2025-08-12 15:20:17 +0900

    feat: invite API 라우트에 Redis 캐시미들웨어 적용

--- src/routes/invite.route.ts
+++ src/routes/invite.route.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import inviteController from "../controllers/invite.controller";
 import authorizeRoles from "../middlewares/authorizeRoles.middleware";
 import authenticateToken from "../middlewares/jwtAuth.middleware";
+import { cacheMiddleware, invalidateCache } from '../middlewares/cacheMiddleware';
 
 const inviteRouter = Router();
 
 inviteRouter.post(
   "/",
+  invalidateCache(),
   authenticateToken,
   authorizeRoles("SUPER_ADMIN"),
   inviteController.createInvite
 );
 
-inviteRouter.get("/:inviteId", inviteController.getInviteInfo);
+inviteRouter.get("/:inviteId", cacheMiddleware(), inviteController.getInviteInfo);
 export default inviteRouter;
