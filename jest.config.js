021a73955c2230f1af05aaaa5576c35fb8174f3c
xdnjs7 <xdnjs77777@gmail.com>
2025-08-14 12:18:04 +0900

    fix: 테스트 실패 문제 해결

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   roots: ['<rootDir>/src'],
+  // 테스트 격리 개선
+  testTimeout: 30000,
+  // 테스트를 순차적으로 실행하여 데드락 방지
+  maxWorkers: 1,
+  // 각 테스트 파일을 독립적으로 실행
+  testSequencer: './test-sequencer.js',
 };
